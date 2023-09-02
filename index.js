52d437a951fb11e1446e539613f582d6ac0a876d
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-09-02 03:45:04 +0100

    add calculatrice

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-let 
+let entrer = document.querySelector('.entrer');
+let touche = document.querySelectorAll('.touche');
+let reset = document.querySelector('.reset');
+for (let i = 0; i < touche.length; i++) {
+    touche[i].addEventListener('click', ()=>{
+        entrer.textContent += touche[i].innerHTML;
+    });
+    
+}
+
+
+reset.addEventListener('click', ()=>{
+    entrer.innerHTML = '';
+})
