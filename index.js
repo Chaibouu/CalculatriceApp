62bce546ffc6359025c63bff24b38722feb9f1d7
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-09-04 00:50:36 +0100

    add calculatrice

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let body = document.body;
 let entrer = document.querySelector('.entrer');
 let touche = document.querySelectorAll('.numero');
 let reset = document.querySelector('.reset');
@@ -7,6 +8,14 @@ let addition = document.querySelector('.addition');
 let soustraction = document.querySelector('.soustraction');
 let multiplication = document.querySelector('.multiplication');
 let division = document.querySelector('.division');
+let themeun = document.querySelectorAll('.btn11');
+let themedeux = document.querySelectorAll('.btn22');
+let themetrois = document.querySelectorAll('.btn33');
+let tchcolor = document.querySelectorAll('.tchcolor');
+let tete = document.querySelector('.tete');
+let conteneur = document.querySelector('.conteneur');
+let bascule = document.querySelector('.bascule');
+
 let a =0;
 let b =0;
 let operateur;
@@ -77,3 +86,76 @@ egal.addEventListener('click', ()=>{
         }
     }
 });
+
+
+
+
+// changer les couleur 
+
+
+themeun.forEach(element => {
+        element.addEventListener('click', ()=>{
+            body.setAttribute('class', 'corps1');
+            tete.classList.remove('tete2');
+            tete.classList.remove('tete3');
+            tete.classList.add('tete1');
+            entrer.classList.remove('entrer3');
+            entrer.classList.remove('entrer2');
+            entrer.classList.add('entrer1');
+            conteneur.classList.remove('conteneur2');
+            conteneur.classList.remove('conteneur3');
+            conteneur.classList.add('conteneur1');
+            bascule.classList.remove('bascule2');
+            bascule.classList.remove('bascule3');
+            bascule.classList.add('bascule1');
+            for (let i = 0; i < tchcolor.length; i++) {
+                tchcolor[i].classList.remove('touche2');
+                tchcolor[i].classList.remove('touche3');
+                tchcolor[i].classList.add('touche1');
+            }
+        });
+})
+themedeux.forEach(element => {
+        element.addEventListener('click', ()=>{
+            body.setAttribute('class', 'corps2');
+            tete.classList.remove('tete1');
+            tete.classList.remove('tete3');
+            tete.classList.add('tete2');
+            entrer.classList.remove('entrer1');
+            entrer.classList.remove('entrer3');
+            entrer.classList.add('entrer2');
+            conteneur.classList.remove('conteneur1');
+            conteneur.classList.remove('conteneur3');
+            conteneur.classList.add('conteneur2');
+            bascule.classList.remove('bascule1');
+            bascule.classList.remove('bascule3');
+            bascule.classList.add('bascule2');
+            for (let i = 0; i < tchcolor.length; i++) {
+                tchcolor[i].classList.remove('touche1');
+                tchcolor[i].classList.remove('touche3');
+                tchcolor[i].classList.add('touche2');
+            }
+        });
+})
+themetrois.forEach(element => {
+        element.addEventListener('click', ()=>{
+            body.setAttribute('class', 'corps3');
+            tete.classList.remove('tete1');
+            tete.classList.remove('tete2');
+            tete.classList.add('tete3');
+            entrer.classList.remove('entrer1');
+            entrer.classList.remove('entrer2');
+            entrer.classList.add('entrer3');
+            conteneur.classList.remove('conteneur1');
+            conteneur.classList.remove('conteneur2');
+            conteneur.classList.add('conteneur3');
+            bascule.classList.remove('bascule1');
+            bascule.classList.remove('bascule2');
+            bascule.classList.add('bascule3');
+            for (let i = 0; i < tchcolor.length; i++) {
+                tchcolor[i].classList.remove('touche2');
+                tchcolor[i].classList.remove('touche1');
+                tchcolor[i].classList.add('touche3');
+            }
+        });
+})
