a7b3e7c2937a6bf9bc93b72990744a06fa57a600
Chaibouu <124494962+Chaibouu@users.noreply.github.com>
2023-11-08 06:18:11 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -18,80 +18,166 @@ let bascule = document.querySelector('.bascule');
 let btn1 = document.querySelector('.btn1');
 let btn2 = document.querySelector('.btn2');
 let btn3 = document.querySelector('.btn3');
+let ope = document.querySelectorAll('.ope');
+let point = document.querySelector('.point');
 
-let a =0;
-let b =0;
-let operateur;
+let a ='';
+let b ='';
+let operateur='';
+let aa=0;
 
 btn1.style.visibility = "visible";
 
+
 // Récupérer les entrer le l'écran
-for (let i = 0; i < touche.length; i++) {
-    touche[i].addEventListener('click', ()=>{
-        entrer.textContent += touche[i].innerHTML;
-    });
-}
-// la touche reset pour effacer la totalité du contenue de l'écran 
-reset.addEventListener('click', ()=>{
-    
-    entrer.innerHTML = '';
-})
-
-// la touche delete pour effacer progressivement les element de l'écran
-del.addEventListener('click', ()=>{
-    let ent = entrer.innerHTML.split("");
-    if (ent.length > 0) {
-        ent.pop();
-        entrer.innerHTML = ent.join("");
-    }
-});
-
-// la fonction pour addition 
-addition.addEventListener('click', ()=>{
-    a = entrer.textContent;
-    entrer.innerHTML = '';
-    operateur = "+";
-});
-
-// la fonction pour soustraction 
-soustraction.addEventListener('click', ()=>{
-    a = entrer.textContent;
-    entrer.innerHTML = '';
-    operateur = "-";
-});
-// la fonction pour multiplication 
-multiplication.addEventListener('click', ()=>{
-    a = entrer.textContent;
-    entrer.innerHTML = '';
-    operateur = "*";
-});
-// la fonction pour division 
-division.addEventListener('click', ()=>{
-    a = entrer.textContent;
-    entrer.innerHTML = '';
-    operateur = "/";
-});
-// la fonction pour egal 
-egal.addEventListener('click', ()=>{
-    b = entrer.textContent;
-    if(operateur=="+"){
-        entrer.innerText = parseFloat(a) + parseFloat(b);
-    }
-    else if(operateur=="-"){
-        entrer.innerText = parseFloat(a) - parseFloat(b);
-    }
-    else if(operateur=="*"){
-        entrer.innerText = parseFloat(a) * parseFloat(b);
-    }
-    else{
-        if(b>0){
-            entrer.innerText = parseFloat(a) / parseFloat(b);
+touche.forEach((touch)=>{
+    touch.addEventListener('click',()=>{
+        if (!operateur) {
+            a += touch.textContent
+            entrer.textContent = a;
+            console.log(a);
         }
         else{
-            entrer.innerText = "ERROR"
+            b += touch.textContent
+            entrer.textContent = b;
+            console.log(b);
         }
+    })
+})
+ope.forEach((ope)=>{
+    ope.addEventListener('click',()=>{
+        if (operateur && b) {
+            switch(operateur){
+                case '+':
+                   aa = aa?  aa + Number(b): Number(a) + Number(b)
+                   entrer.textContent = aa;
+                   b = '';
+                   a = ''
+                break;
+                case '-':
+                   aa = aa?  aa - Number(b): Number(a) - Number(b)
+                   entrer.textContent = aa;
+                   b = '';
+                   a = ''
+                break;
+                case '×':
+                   aa = aa?  aa * Number(b): Number(a) * Number(b)
+                   entrer.textContent = aa;
+                   b = '';
+                   a = ''
+                break;
+                case '/':
+                   aa = aa?  aa / Number(b): Number(a) / Number(b)
+                   entrer.textContent = aa;
+                   b = '';
+                   a = ''
+                break;
+                case '=':
+                    entrer.textContent = aa;
+                break;
+            }
+        }
+        else{
+            operateur = ope.textContent
+            console.log(operateur);
+        }
+    })
+})
+point.addEventListener('click', function(){
+    if (!a) {
+        a = '0.'
+        entrer.textContent = a;
+        console.log(a);
     }
-});
+    else if (a && !a.includes('.')){
+        a += '.'
+        entrer.textContent = a;
+        console.log(a);
+    }
+    // if (!b) {
+    //     b = '0.'
+    //     entrer.textContent = b;
+    //     console.log(b);
+    // }
+    // else if (b && !b.includes('.')){
+    //     b += '.'
+    //     entrer.textContent = b;
+    //     console.log(b);
+    // }
+})
+// // Récupérer les entrer le l'écran
+// for (let i = 0; i < touche.length; i++) {
+//     touche[i].addEventListener('click', ()=>{
+//         // entrer.textContent += touche[i].textContent;
+//         a = touche[i].textContent;
+//     });
+// }
+// // la touche reset pour effacer la totalité du contenue de l'écran 
+// reset.addEventListener('click', ()=>{
+    
+//     entrer.innerHTML = '';
+// })
+
+// // la touche delete pour effacer progressivement les element de l'écran
+// del.addEventListener('click', ()=>{
+//     let ent = entrer.innerHTML.split("");
+//     if (ent.length > 0) {
+//         ent.pop();
+//         entrer.innerHTML = ent.join("");
+//     }
+// });
+
+// // la fonction pour addition 
+// addition.addEventListener('click', ()=>{
+//     if (!operateur) {
+//         a = entrer.textContent;
+//         operateur = "+";
+//     }
+//     else{
+//         b = entrer.textContent;
+//     }
+   
+// });
+
+// // la fonction pour soustraction 
+// soustraction.addEventListener('click', ()=>{
+//     a = entrer.textContent;
+//     entrer.innerHTML = '';
+//     operateur = "-";
+// });
+// // la fonction pour multiplication 
+// multiplication.addEventListener('click', ()=>{
+//     a = entrer.textContent;
+//     entrer.innerHTML = '';
+//     operateur = "*";
+// });
+// // la fonction pour division 
+// division.addEventListener('click', ()=>{
+//     a = entrer.textContent;
+//     entrer.innerHTML = '';
+//     operateur = "/";
+// });
+// // la fonction pour egal 
+// egal.addEventListener('click', ()=>{
+//     b = entrer.textContent;
+//     if(operateur=="+"){
+//         entrer.innerText = parseFloat(a) + parseFloat(b);
+//     }
+//     else if(operateur=="-"){
+//         entrer.innerText = parseFloat(a) - parseFloat(b);
+//     }
+//     else if(operateur=="*"){
+//         entrer.innerText = parseFloat(a) * parseFloat(b);
+//     }
+//     else{
+//         if(b>0){
+//             entrer.innerText = parseFloat(a) / parseFloat(b);
+//         }
+//         else{
+//             entrer.innerText = "ERROR"
+//         }
+//     }
+// });
 
 
 
